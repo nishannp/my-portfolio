fb52b4cdfcf35a432291a8287cad78c78c3f4f3a
snav <nishan.snav@gmail.com>
2024-05-22 21:28:06 +0545

    new

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './navbar.css'; // Import the CSS file
+import './navbar.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,22 +14,32 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">My Portfolio</Link>
+        <Link to="/" className="navbar-logo">
+          MyPortfolio
+        </Link>
         <div className="menu-icon" onClick={toggleMenu}>
           <i className={isOpen ? 'fas fa-times' : 'fas fa-bars'}></i>
         </div>
         <ul className={isOpen ? 'nav-menu active' : 'nav-menu'}>
           <li className="nav-item">
-            <Link to="/" className="nav-links" onClick={toggleMenu}>Home</Link>
+            <Link to="/" className="nav-links" onClick={toggleMenu}>
+              Home
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/gallery" className="nav-links" onClick={toggleMenu}>Gallery</Link>
+            <Link to="/gallery" className="nav-links" onClick={toggleMenu}>
+              Gallery
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/projects" className="nav-links" onClick={toggleMenu}>Projects</Link>
+            <Link to="/projects" className="nav-links" onClick={toggleMenu}>
+              Projects
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/contact" className="nav-links" onClick={toggleMenu}>Contact</Link>
+            <Link to="/contact" className="nav-links" onClick={toggleMenu}>
+              Contact
+            </Link>
           </li>
         </ul>
       </div>
